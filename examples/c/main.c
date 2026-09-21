d45962c6c777fdf58ae28d7e2ff9f628e2a17861
Erich Seifert <dev@erichseifert.de>
2026-09-21 13:17:01 +0200

    Inspect imported representations in C example

--- examples/c/main.c
+++ examples/c/main.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv) {
 
   pp_production_t *production = NULL;
   pp_transaction_t *transaction = NULL;
+  pp_representation_set_t *representations = NULL;
   pp_error_t *error = NULL;
   pp_uuid_t asset_id = {{0}};
   pp_error_code_t status = pp_production_open(argv[1], &production, &error);
@@ -30,14 +31,21 @@ int main(int argc, char **argv) {
   if (status == PP_OK) {
     status = pp_transaction_commit(transaction, &error);
   }
+  if (status == PP_OK) {
+    status = pp_production_representations(production, &asset_id,
+                                           &representations, &error);
+  }
 
   if (status != PP_OK) {
     fprintf(stderr, "operation failed (%u): %s\n", status,
             error != NULL ? pp_error_message(error) : "no details");
   } else {
     print_uuid(&asset_id);
+    printf("representations: %llu\n",
+           (unsigned long long)pp_representation_set_count(representations));
   }
   pp_error_release(error);
+  pp_representation_set_release(representations);
   pp_transaction_release(transaction);
   pp_production_release(production);
   return status == PP_OK ? 0 : 1;
