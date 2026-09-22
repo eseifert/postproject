1b3fea098c536f1c85a4deb97a278d58fd8301da
Erich Seifert <dev@erichseifert.de>
2026-09-22 18:58:12 +0200

    Make the C++ example import media

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -4,16 +4,19 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "usage: postproject-cpp-example PRODUCTION\n";
+  if (argc != 3) {
+    std::cerr
+        << "usage: postproject-cpp-example OUTPUT_PRODUCTION MEDIA_FILE\n";
     return 2;
   }
 
   try {
-    const auto production = postproject::Production::open(argv[1]);
-    const auto id = production.id();
-    std::cout << "opened production with " << id.bytes().size()
-              << " identity bytes\n";
+    auto production = postproject::Production::create(argv[1], "C++ quickstart");
+    auto transaction = production.beginTransaction();
+    const auto asset_id = transaction.importMedia(argv[2], "Quickstart media");
+    transaction.commit();
+    std::cout << "representations: "
+              << production.representations(asset_id).size() << '\n';
   } catch (const postproject::Error &error) {
     std::cerr << error.what() << '\n';
     return 1;
